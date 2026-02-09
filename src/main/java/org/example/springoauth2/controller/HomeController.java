34707bb43580e465675bc76ce7b5113c3fbea87f
hebcodee <herberth.ferreira6@gmail.com>
2026-02-09 14:33:01 -0300

    feat: add HomeController

---
+++ src/main/java/org/example/springoauth2/controller/HomeController.java
@@ -0,0 +1,17 @@
+package org.example.springoauth2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "Home Page!";
+    }
+
+    @GetMapping("/secured")
+    public String secured(){
+        return "Page Secured";
+    }
+}
